180c677e80f59aedd3cc60e9f6c59f99ba0fe3eb
liwenpeng022 <1031043200@qq.com>
2020-06-09 17:12:43 +0800

    提交到本地仓库

---
+++ git-project/src/com/git/dao/UserDao.java
@@ -0,0 +1,23 @@
+package com.git.dao;
+
+public class UserDao {
+ 
+	public void addUser(int id,String username,String password) {
+		
+	}
+	public void deleteUser(int id) {
+		
+	}
+	
+	public void updateUser(int id,String username,String password) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
